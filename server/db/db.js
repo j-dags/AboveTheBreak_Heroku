70b279da4192d97e332dc48536d3d069697faa62
j-dags <jon.dagdagan@gmail.com>
2021-03-18 20:03:33 -0500

    ssl

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,30 @@
 const Sequelize = require('sequelize')
 
-const db = new Sequelize(
-	process.env.DATABASE_URL || `postgres://localhost:5432/abovethebreak`,
-	{
+let config
+
+if (process.env.DATABASE_URL) {
+	config = {
+		logging: false,
+		operatorsAliases: false,
+		dialect: 'postgres',
+		protocol: 'postgres',
+		ssl: true,
+		dialectOptions: {
+			ssl: {
+				require: true,
+				rejectUnauthorized: false,
+			},
+		},
+	}
+} else {
+	config = {
 		logging: false,
 	}
+}
+
+const db = new Sequelize(
+	process.env.DATABASE_URL || `postgres://localhost:5432/abovethebreak`,
+	config
 )
 
 module.exports = db
